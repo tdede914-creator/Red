cdcf5104612a1799684ecc3cbd16fc93ec9476a9
bowowiwendi <44046292+bowowiwendi@users.noreply.github.com>
2024-03-07 12:36:59 +0700

    Update cf.sh

--- files/cf.sh
+++ files/cf.sh
@@ -4,21 +4,20 @@ apt install jq curl -y
 clear
 echo -e ""
 echo -e "\033[96;1m============================\033[0m"
-echo -e "\033[93;1m      INPUT SUNDOMAIN"
+echo -e "\033[93;1m      INPUT SUBDOMAIN"
 echo -e "\033[96;1m============================\033[0m"
 echo -e "\033[91;1m Note. contoh Subdomain :\033[0m \033[93mbodoh22 \033[0m"
 echo -e " "
 read -p "SUBDOMAIN :  " domen
 echo -e ""
 #DOMAIN=mypremium.biz.id
-DOMAIN=zvx.my.id
+DOMAIN=wendivpn.my.id
 sub=${domen}
-dns=${sub}.zvx.my.id
+dns=${sub}.wendivpn.my.id
 #(</dev/urandom tr -dc a-z0-9 | head -c5)
 #dns=${sub}.mypremium.biz.id
 #CF_ID=andyyuda41@gmail.com
 #CF_KEY=0d626234700bad388d6d07b49c42901445d1c
-dns=${sub}.zvx.my.id
 CF_KEY=e03f30d53ad7ec2ab54327baa5e2da5ab44f0
 CF_ID=mezzqueen293@gmail.com
 set -euo pipefail
