b33379e4a7beea11571608d45e98173b49a701fb
bowowiwendi <44046292+bowowiwendi@users.noreply.github.com>
2024-03-08 13:07:49 +0700

    Update update.sh

--- files/update.sh
+++ files/update.sh
@@ -51,7 +51,7 @@ rm -rf update.sh
 netfilter-persistent
 clear
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e " \e[1;97;101m          UPDATE SCRIPT       \e[0m"
+echo -e " \e[1;97;101m             UPDATE SCRIPT             \e[0m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 echo -e "  \033[1;91m update script service\033[1;37m"
