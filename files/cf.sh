a546146a5c69e9827b657bcb97f79e67610a9852
bowowiwendi <44046292+bowowiwendi@users.noreply.github.com>
2024-03-31 18:11:27 +0700

    Add files via upload

--- files/cf.sh
+++ files/cf.sh
@@ -7,7 +7,7 @@ clear
 #CF_ID=ridwanstoreaws@gmail.com
 #CF_KEY=4ecfe9035f4e6e60829e519bd5ee17d66954f
 function auto-dns(){
-sub=sc-`</dev/urandom tr -dc a-z0-9 | head -c5`
+sub=WV-`</dev/urandom tr -dc a-z0-9 | head -c5`
 DOMAIN=vpn-prem.biz.id
 SUB_DOMAIN=${sub}.vpn-prem.biz.id
 CF_ID=padliapandi459@gmail.com
@@ -40,10 +40,12 @@ RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_r
      --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
 
 echo "$SUB_DOMAIN" > /root/domain
-rm -f /root/cf.sh
-echo -e "Selesai"
-read -p "press !! [ ENTER ] To menu"
-menu
+echo "$SUB_DOMAIN" > /root/scdomain
+echo "$SUB_DOMAIN" > /etc/xray/domain
+echo "$SUB_DOMAIN" > /etc/v2ray/domain
+echo "$SUB_DOMAIN" > /etc/xray/scdomain
+echo "IP=$SUB_DOMAIN" > /var/lib/kyt/ipvps.conf
+cd
 }
 function dns(){
 read -rp "Masukan Subdomain kamu (Contoh: Wendi99): " -e sub
@@ -77,46 +79,28 @@ RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_r
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" \
      --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
-
+     
 echo "$SUB_DOMAIN" > /root/domain
-rm -f /root/cf.sh
-echo -e "Selesai"
-read -p "press !! [ ENTER ] To menu"
-menu
+echo "$SUB_DOMAIN" > /root/scdomain
+echo "$SUB_DOMAIN" > /etc/xray/domain
+echo "$SUB_DOMAIN" > /etc/v2ray/domain
+echo "$SUB_DOMAIN" > /etc/xray/scdomain
+echo "IP=$SUB_DOMAIN" > /var/lib/kyt/ipvps.conf
+cd
 }
-
+clear
 echo -e "\033[96m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-echo -e " \e[1;971m                   MENU EXTRA                        \e[0m"
+echo -e " \e[1;971m                    ADD DOMAIN                        \e[0m"
 echo -e "\033[96m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-echo -e "  ${grenbo}[1]${NC}${YELL} MENU NOOBZ ${NC}"
-echo -e "  ${grenbo}[2]${NC}${YELL} Auto Change Sub DNS ${NC}"
-echo -e "  ${grenbo}[3]${NC}${YELL} Manual Change Sub DNS ${NC}"
-echo -e "  ${grenbo}[4]${NC}${YELL} Install SLW DNS ${NC}"
-echo -e "  ${grenbo}[5]${NC}${YELL} MENU THEME ${NC}"
+echo -e "  ${grenbo}[2]${NC}${YELL} Auto Sub Domain ${NC}"
+echo -e "  ${grenbo}[3]${NC}${YELL} Manual Sub Domain ${NC}"
 echo -e ""
-echo -e "  ${grenbo}[0]${NC}${YELL} Back To Menu${NC}"
-echo -e ""
-read -p "  Select From Options [ 1 - 3 or 0 ] : " menu
+read -p "  Select From Options [ 1 - 2 ] : " menu
 case $menu in
 1) clear ;
-    noobz
-    ;;
-2) clear ;
     auto-dns
     ;;
-3) clear ;
+2) clear ;
     dns
-    ;;
-4) clear ;
-    wget https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/SLDNS/install-sldns && chmod +x install-sldns && ./install-sldns
-    ;;
-5) clear ;
-    menu-theme.sh
-    ;;
-0) clear ;
-    menu
-    ;;
-*) clear ;
-    menu
     ;;
 esac
