7dc8f840ec418372e85ec5f311d263f747e7334a
bowowiwendi <44046292+bowowiwendi@users.noreply.github.com>
2024-05-11 20:42:15 +0700

    Update limit.sh

--- files/limit.sh
+++ files/limit.sh
@@ -6,16 +6,20 @@ wget -q -O /etc/systemd/system/limitvmess.service "${REPO}files/limitvmess.servi
 wget -q -O /etc/systemd/system/limitvless.service "${REPO}files/limitvless.service" && chmod +x limitvless.service >/dev/null 2>&1
 wget -q -O /etc/systemd/system/limittrojan.service "${REPO}files/limittrojan.service" && chmod +x limittrojan.service >/dev/null 2>&1
 wget -q -O /etc/systemd/system/limitshadowsocks.service "${REPO}files/limitshadowsocks.service" && chmod +x limitshadowsocks.service >/dev/null 2>&1
+wget -q -O /etc/systemd/system/ssh.service "${REPO}files/ssh.service" && chmod +x limitssh.service >/dev/null 2>&1
 wget -q -O /etc/xray/limit.vmess "${REPO}files/vmess" >/dev/null 2>&1
 wget -q -O /etc/xray/limit.vless "${REPO}files/vless" >/dev/null 2>&1
 wget -q -O /etc/xray/limit.trojan "${REPO}files/trojan" >/dev/null 2>&1
 wget -q -O /etc/xray/limit.shadowsocks "${REPO}files/shadowsocks" >/dev/null 2>&1
+wget -q -O /etc/xray/limit.ssh "${REPO}files/ssh" >/dev/null 2>&1
 chmod +x /etc/xray/limit.vmess
 chmod +x /etc/xray/limit.vless
 chmod +x /etc/xray/limit.trojan
 chmod +x /etc/xray/limit.shadowsocks
+chmod +x /etc/xray/limit.ssh
 systemctl daemon-reload
 systemctl enable --now limitvmess
 systemctl enable --now limitvless
 systemctl enable --now limittrojan
 systemctl enable --now limitshadowsocks
+systemctl enable --now limitssh
