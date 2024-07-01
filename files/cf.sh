c21e978ab32f11eb0382837f8e773e08c2b909f0
Wendi <44046292+bowowiwendi@users.noreply.github.com>
2024-07-02 01:08:54 +0700

    Update cf.sh

--- files/cf.sh
+++ files/cf.sh
@@ -4,11 +4,11 @@ apt install jq curl -y
 clear
 echo -e ""
 echo -e "\033[96;1m============================\033[0m"
-echo -e "\033[93;1m      INPUT SUNDOMAIN"
+echo -e "\033[93;1m      INPUT DOMAIN"
 echo -e "\033[96;1m============================\033[0m"
 echo -e "\033[91;1m Note. contoh Subdomain :\033[0m \033[93mWendi99 \033[0m"
 echo -e " "
-read -p "SUBDOMAIN :  " domen
+read -p "DOMAIN :  " domen
 echo -e ""
 #DOMAIN=mypremium.biz.id
 sub=${domen}
