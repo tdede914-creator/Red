af4232a74b9e1c119c30a9fa05059f474b60b6a8
Wendi <44046292+bowowiwendi@users.noreply.github.com>
2024-07-20 12:33:42 +0700

    Add files via upload

--- setup-main.sh
+++ setup-main.sh
@@ -168,25 +168,46 @@ timedatectl set-timezone Asia/Jakarta
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections
 print_success "Directory Xray"
-if [[ $(cat /etc/os-release | grep -w ID | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/ID//g') == "ubuntu" ]]; then
-echo "Setup Dependencies $(cat /etc/os-release | grep -w PRETTY_NAME | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/PRETTY_NAME//g')"
-sudo apt update -y
-apt-get install --no-install-recommends software-properties-common
-add-apt-repository ppa:vbernat/haproxy-2.0 -y
-apt-get -y install haproxy=2.0.\*
-elif [[ $(cat /etc/os-release | grep -w ID | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/ID//g') == "debian" ]]; then
-echo "Setup Dependencies For OS Is $(cat /etc/os-release | grep -w PRETTY_NAME | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/PRETTY_NAME//g')"
-curl https://haproxy.debian.net/bernat.debian.org.gpg |
-gpg --dearmor >/usr/share/keyrings/haproxy.debian.net.gpg
-echo deb "[signed-by=/usr/share/keyrings/haproxy.debian.net.gpg]" \
-http://haproxy.debian.net buster-backports-1.8 main \
->/etc/apt/sources.list.d/haproxy.list
-sudo apt-get update
-apt-get -y install haproxy=1.8.\*
-else
-echo -e " Your OS Is Not Supported ($(cat /etc/os-release | grep -w PRETTY_NAME | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/PRETTY_NAME//g') )"
-exit 1
-fi
+OS_ID=$(cat /etc/os-release | grep -w ID | head -n1 | sed 's/ID=//g' | sed 's/"//g')
+VERSION_ID=$(cat /etc/os-release | grep -w VERSION_ID | head -n1 | sed 's/VERSION_ID=//g' | sed 's/"//g')
+case $OS_ID in
+    ubuntu|debian)
+        echo "Setup Dependencies for $OS_ID"
+        sudo apt update -y
+        apt-get install --no-install-recommends software-properties-common
+        if [[ $OS_ID == "ubuntu" ]]; then
+            add-apt-repository ppa:vbernat/haproxy-2.0 -y
+            apt-get -y install haproxy=2.0.\*
+        else
+            if [[ $VERSION_ID == "11" ]]; then
+                curl https://haproxy.debian.net/bernat.debian.org.gpg | gpg --dearmor >/usr/share/keyrings/haproxy.debian.net.gpg
+                echo deb "[signed-by=/usr/share/keyrings/haproxy.debian.net.gpg]" http://haproxy.debian.net bullseye-backports-2.4 main >/etc/apt/sources.list.d/haproxy.list
+                sudo apt-get update
+                apt-get -y install haproxy=2.4.\*
+            else
+                curl https://haproxy.debian.net/bernat.debian.org.gpg | gpg --dearmor >/usr/share/keyrings/haproxy.debian.net.gpg
+                echo deb "[signed-by=/usr/share/keyrings/haproxy.debian.net.gpg]" http://haproxy.debian.net buster-backports-1.8 main >/etc/apt/sources.list.d/haproxy.list
+                sudo apt-get update
+                apt-get -y install haproxy=1.8.\*
+            fi
+        fi
+        ;;
+    centos|fedora|rhel)
+        echo "Setup Dependencies for $OS_ID"
+        sudo yum update -y
+        sudo yum install -y epel-release
+        sudo yum install -y haproxy
+        ;;
+    arch)
+        echo "Setup Dependencies for $OS_ID"
+        sudo pacman -Syu --noconfirm
+        sudo pacman -S --noconfirm haproxy
+        ;;
+    *)
+        echo -e "Your OS Is Not Supported ($OS_Name)"
+        exit 1
+        ;;
+esac
 }
 clear
 function nginx_install() {
