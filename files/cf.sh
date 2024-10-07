48bb64ca42f18e0d9f83687747dee05b3b0e89be
Wendi <44046292+bowowiwendi@users.noreply.github.com>
2024-10-07 09:57:31 +0700

    Update cf.sh

--- files/cf.sh
+++ files/cf.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
-MYIP=$(wget -qO- icanhazip.com);
 apt install jq curl -y
-clear
-echo -e ""
-echo -e "\033[96;1m============================\033[0m"
-echo -e "\033[93;1m      INPUT DOMAIN N SUB/WILCARD "
-echo -e "\033[96;1m============================\033[0m"
-echo -e "\033[91;1m contoh domain :\033[0m \033[93sshprem.cloud\033[0m"
-echo -e "contoh subdomain : wendivpn"
-read -p "DOMAIN :  " domain
-read -p "SUB :  " sub
-echo -e ""
-wilcard=*.${sub}.${domain}
-#(</dev/urandom tr -dc a-z0-9 | head -c5)
-dns=${sub}.${domain}
+domain=itachi.cyou
+sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
+subsl=$(</dev/urandom tr -dc a-x0-9 | head -c5)
+dns=${sub}.itachi.cyou
+wilcard=*.${dns}
+nsdomain=ns-${subsl}.itachi.cyou
+IP=$(wget -qO- icanhazip.com)
 CF_KEY=dc7a32077573505cc082f4be752509a5c5a3e
 CF_ID=bowowiwendi@gmail.com
 set -euo pipefail
-IP=$(wget -qO- icanhazip.com);
 #domain
 echo "Updating DNS for ${dns}..."
 ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${domain}&status=active" \
@@ -67,11 +59,44 @@ RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_r
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" \
      --data '{"type":"A","name":"'${wilcard}'","content":"'${IP}'","ttl":120,"proxied":false}')
+#Nameserver
+echo "Updating DNS NS for ${nsdomain}..."
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${domain}&status=active" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
 
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${nsdomain}" \  # Perbaiki kesalahan penutupan kurung kurawal
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+if [[ "${#RECORD}" -le 10 ]]; then
+     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"NS","name":"'${nsdomain}'","content":"'${dns}'","ttl":120,"proxied":false}' | jq -r .result.id)
+fi
+
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"NS","name":"'${nsdomain}'","content":"'${dns}'","ttl":120,"proxied":false}')
+clear
+echo -e ""
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo -e "  DOMAIN RANDOM N NS RANDOM "
+echo -e " 🍀SUCCESSFULLY POINTING🍀 "
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo -e "SUBDOMAIN    : $dns" 
+echo -e "WILCARD      : $wilcard" 
+echo -e "NAMESERVER   : $nsdomain" 
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 echo "$dns" > /root/domain
 echo "$dns" > /root/scdomain
 echo "$dns" > /etc/xray/domain
 echo "IP=$dns" > /var/lib/kyt/ipvps.conf
-echo $nsdomain > /root/nsdomain
+echo "$nsdomain" > /root/nsdomain
 cd
-
