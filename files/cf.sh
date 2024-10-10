9490cfb3dd77ea648b985fddca7772a5e3a32af8
Wendi <44046292+bowowiwendi@users.noreply.github.com>
2024-10-10 12:21:18 +0700

    Update cf.sh

--- files/cf.sh
+++ files/cf.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 apt install jq curl -y
-domain=itachi.cyou
+domain=ssh-prem.xyz
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
-dns=${sub}.${domain}
-wildcard=*.${dns}
-nsdomain=ns-${dns}
 IP=$(wget -qO- icanhazip.com)
 CF_KEY=dc7a32077573505cc082f4be752509a5c5a3e
 CF_ID=bowowiwendi@gmail.com
+dns=${sub}.${domain}
+wilcard=*.${dns}
 set -euo pipefail
 #domain
 echo "Updating DNS for ${dns}..."
@@ -34,13 +33,13 @@ RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_r
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" \
      --data '{"type":"A","name":"'${dns}'","content":"'${IP}'","ttl":120,"proxied":false}')
-#wildcard
+#wilcard
 ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${domain}&status=active" \
      -H "X-Auth-Email: ${CF_ID}" \
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" | jq -r .result[0].id)
 
-RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${wildcard}" \
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${wilcard}" \
      -H "X-Auth-Email: ${CF_ID}" \
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" | jq -r .result[0].id)
@@ -50,44 +49,19 @@ if [[ "${#RECORD}" -le 10 ]]; then
      -H "X-Auth-Email: ${CF_ID}" \
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" \
-     --data '{"type":"A","name":"'${wildcard}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
+     --data '{"type":"A","name":"'${wilcard}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
 fi
 
 RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
      -H "X-Auth-Email: ${CF_ID}" \
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" \
-     --data '{"type":"A","name":"'${wildcard}'","content":"'${IP}'","ttl":120,"proxied":false}')
-#Nameserver
-echo "Updating DNS NS for ${nsdomain}..."
-ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${domain}&status=active" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" | jq -r .result[0].id)
-
-RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${nsdomain}" \ 
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" | jq -r .result[0].id)
-
-if [[ "${#RECORD}" -le 10 ]]; then
-     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" \
-     --data '{"type":"NS","name":"'${nsdomain}'","content":"'${dns}'","ttl":120,"proxied":false}' | jq -r .result.id)
-fi
-
-RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" \
-     --data '{"type":"NS","name":"'${nsdomain}'","content":"'${dns}'","ttl":120,"proxied":false}')
+     --data '{"type":"A","name":"'${wilcard}'","content":"'${IP}'","ttl":120,"proxied":false}')
+     
 echo $dns > /etc/xray/scdomain
 echo $dns > /etc/v2ray/domain
 echo $dns > /root/domain
 echo $dns > /root/scdomain
 echo $dns > /etc/xray/domain
-echo "IP=" > /var/lib/kyt/ipvps.conf
-echo $nsdomain > /root/nsdomain
+echo "IP=" >> /var/lib/kyt/ipvps.conf
 cd
