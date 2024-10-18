0c6115f6990c21232f520259f1f9da1c30b1bf12
Wendi <44046292+bowowiwendi@users.noreply.github.com>
2024-10-18 10:24:01 +0700

    Update setup-main.sh

--- setup-main.sh
+++ setup-main.sh
@@ -295,7 +295,6 @@ TEXT="
 <code>ID     : </code><code>$USRSC</code>
 <code>Domain : </code><code>$domain</code>
 <code>Wilcard: </code><code>*.$domain</code>
-<code>NsDomain : </code><code>dns.$domain</code>
 <code>Date   : </code><code>$TIME</code>
 <code>Time   : </code><code>$TIMEZONE</code>
 <code>Ip vps : </code><code>$MYIP</code>
