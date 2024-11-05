d5a081559a1f26a4f99f451b0bc7cbefb77c7490
Wendi <44046292+bowowiwendi@users.noreply.github.com>
2024-11-05 07:14:45 +0700

    Update

--- setup-main2.sh
+++ setup-main2.sh
@@ -705,7 +705,7 @@ print_success "All Packet"
 function menu(){
 clear
 print_install "Memasang Menu Packet"
-wget ${REPO}Features/menu.zip
+wget ${REPO}Features/menu1.zip
 unzip menu.zip
 chmod +x menu/*
 mv menu/* /usr/local/sbin
