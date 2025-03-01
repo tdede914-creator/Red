396f5be6781592f3284806de6b6e7df460231912
Wendi <44046292+bowowiwendi@users.noreply.github.com>
2025-03-02 06:49:34 +0700

    Update setup-main.sh

--- setup-main.sh
+++ setup-main.sh
@@ -751,7 +751,7 @@ cat << EOF >> /etc/crontab
 EOF
 cat << EOF >> /etc/crontab
 # BEGIN_Del
-0 6 * * * root xp
+0 3 * * * root xp
 # END_Del
 EOF
 cat >/etc/cron.d/logclean <<-END
@@ -763,7 +763,7 @@ chmod 644 /root/.profile
 cat >/etc/cron.d/daily_reboot <<-END
 SHELL=/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-0 5 * * * root /sbin/reboot
+0 3 * * * root /sbin/reboot
 END
 echo "*/1 * * * * root echo -n > /var/log/nginx/access.log" >/etc/cron.d/log.nginx
 echo "*/1 * * * * root echo -n > /var/log/xray/access.log" >>/etc/cron.d/log.xray
