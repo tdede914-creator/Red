c70c8845b16213d3f5b25270d78e139ba0eee542
tdede914-creator <tdede914@gmail.com>
2025-08-29 00:30:52 +0700

    Update bot.sh

--- bot/bot.sh
+++ bot/bot.sh
@@ -10,13 +10,13 @@ NC='\e[0m'
 apt update && apt upgrade
 apt install python3 python3-pip git
 cd /usr/bin
-wget https://raw.githubusercontent.com/gemilangvip/autoscript-free/main/bot/bot.zip
+wget https://raw.githubusercontent.com/Dikajuana/botvpn/tree/main/bot/bot.zip
 unzip bot.zip
 mv bot/* /usr/bin
 chmod +x /usr/bin/*
 rm -rf bot.zip
 clear
-wget https://raw.githubusercontent.com/gemilangvip/autoscript-free/main/bot/kyt.zip
+wget https://raw.githubusercontent.com/Dikajuana/botvpn/tree/main/bot/kyt.zip
 unzip kyt.zip
 pip3 install -r kyt/requirements.txt
 
